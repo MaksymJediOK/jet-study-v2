9b29b21d876e1c6f3be7f43fb0a34acf2d95cc23
MaksymJediOK <heckylmoon@gmail.com>
2024-02-22 21:06:35 +0200

    feat: lector info component

---
+++ components/AccordionList/components/LectorInfo.tsx
@@ -0,0 +1,15 @@
+const LectorInfo = () => {
+  return (
+    <div className='mb-4 flex flex-col gap-1'>
+      <h4 className='scroll-m-20 text-base font-medium leading-7 tracking-tight underline'>
+        Yurii Kleban
+      </h4>
+      <p className='leading-[26px]'>
+        Розробник компанії SoftServe з багаторічним досвідом. Працював над такими продуктами як Rozetka
+        та Headway.
+      </p>
+    </div>
+  )
+}
+
+export { LectorInfo }
