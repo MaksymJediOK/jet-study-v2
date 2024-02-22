95076e801ab9c049d7a08cf0ac5d4678a97b7bb3
MaksymJediOK <heckylmoon@gmail.com>
2024-02-22 21:07:26 +0200

    feat: text shortener

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,7 @@
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
+export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs))
+
+export const shortText = (text: string, end: number) =>
+  text.length > end ? text.slice(0, end).concat('...') : text
