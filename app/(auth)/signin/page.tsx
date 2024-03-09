baf1e4a98945f892d1f6bb84d3e2841940b99d51
MaksymJediOK <heckylmoon@gmail.com>
2024-03-09 19:42:44 +0200

    feat: oauth additions

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,9 +1,17 @@
+'use client'
+
 import Link from 'next/link'
 import Image from 'next/image'
 import { UserAuthForm } from '@/components/UserAuthForm'
 import { Button } from '@/components/ui/button'
+import { useGoogleLogin } from '@react-oauth/google'
 
 export default function SignInPage() {
+  const login = useGoogleLogin({
+    onSuccess: (tokenResponse) => console.log(tokenResponse),
+    onError: (errorResponse) => console.log(errorResponse),
+  })
+
   return (
     <>
       <h3 className='w-full scroll-m-10 text-[28px] font-semibold leading-9 tracking-tighter'>
@@ -15,7 +23,11 @@ export default function SignInPage() {
           Реєстрація
         </Link>
       </p>
-      <Button className='mt-4 flex w-[95%] items-center justify-center gap-2  px-4 py-2 '>
+      <Button
+        variant='outline'
+        className='mt-4 flex w-[95%] items-center justify-center gap-2  px-4 py-2'
+        onClick={() => login()}
+      >
         <Image src='/google.svg' width={16} height={16} alt='google icon' />
         <span className='text-sm font-medium leading-6 text-[#0F172A] '>Увійти через Google </span>
       </Button>
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,9 +1,17 @@
-import Link from 'next/link'
+'use client'
 
+import Link from 'next/link'
 import Image from 'next/image'
 import { UserAuthForm } from '@/components/UserAuthForm'
+import { useGoogleLogin } from '@react-oauth/google'
+import { Button } from '@/components/ui/button'
 
 export default function SignUpPage() {
+  const login = useGoogleLogin({
+    onSuccess: (tokenResponse) => console.log(tokenResponse),
+    onError: (errorResponse) => console.log(errorResponse),
+  })
+
   return (
     <>
       <h3 className='w-full scroll-m-10 text-[28px] font-semibold leading-9 tracking-tighter'>
@@ -15,15 +23,16 @@ export default function SignUpPage() {
           Увійти
         </Link>
       </p>
-      <button
-        className='mt-4 flex w-[95%] items-center
-      justify-center gap-2 rounded-md
-      border border-black px-4
-      py-2 ring-offset-background transition-colors focus-visible:outline-none'
+      <Button
+        variant='outline'
+        className='mt-4 flex w-full items-center justify-center gap-2  px-4 py-2'
+        onClick={() => login()}
       >
         <Image src='/google.svg' width={16} height={16} alt='google icon' />
-        <span className='text-sm font-medium leading-6 text-[#0F172A] '>Зареєструватись з Google </span>
-      </button>
+        <span className='text-sm font-medium leading-6 text-[#0F172A] '>
+          Зареєструватись через Google{' '}
+        </span>
+      </Button>
       <h4 className='my-3 text-center text-base leading-7 tracking-tighter opacity-50'>Або</h4>
       <UserAuthForm />
     </>
