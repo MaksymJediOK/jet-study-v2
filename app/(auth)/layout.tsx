93294e11fc0d736e998134268e2e18e88382084e
MaksymJediOK <heckylmoon@gmail.com>
2024-03-09 19:45:45 +0200

    feat(auth): oauth wrapper

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {GoogleOAuthProvider} from '@react-oauth/google'
 
 type AuthLayout = {
   children?: React.ReactNode
@@ -6,8 +7,10 @@ type AuthLayout = {
 
 export default async function AuthLayout({ children }: AuthLayout) {
   return (
-    <div className='flex h-screen items-center justify-center'>
-      <div className='container w-[310px] p-6'>{children}</div>
-    </div>
+    <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_CLIENT_ID || ''}>
+      <div className='flex h-screen items-center justify-center'>
+        <div className='container w-[310px] p-6'>{children}</div>
+      </div>
+    </GoogleOAuthProvider>
   )
 }
