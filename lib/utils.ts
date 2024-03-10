9e09bb0864491a3f4f89fbee08eb9e14e73399e9
MaksymJediOK <heckylmoon@gmail.com>
2024-03-10 23:03:52 +0200

    fix: remove unused import

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,6 @@
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
-import { FilterParams, FilterRequestParams } from '@/types'
+import { FilterRequestParams } from '@/types'
 
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs))
 
